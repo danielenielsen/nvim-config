d545cc1c2b738e967606c849b399d92e20e50ec4
Daniel Ellebæk Nielsen <daniogdan@gmail.com>
2024-11-24 17:40:56 +0100

    More harpoon shortcuts

--- init.lua
+++ init.lua
@@ -3,4 +3,5 @@ require("config.lazy")
 require("config.options")
 require("config.lsp-config")
 require("config.telescope")
+require("config.harpoon")
 
--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -15,6 +15,11 @@ return {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
   }
 }
 
